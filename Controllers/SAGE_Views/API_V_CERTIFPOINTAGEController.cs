d2ec62537b7a4ba62fb26d61df4e3269020bec57
Administrator <Administrator@PERSOVPS>
2025-11-23 12:01:57 +0100

    231125_1201

--- Controllers/SAGE_Views/API_V_CERTIFPOINTAGEController.cs
+++ Controllers/SAGE_Views/API_V_CERTIFPOINTAGEController.cs
@@ -42,13 +42,13 @@ namespace BusinessWeb.Controllers.SAGE_Tables
         [HttpGet]
         public async Task<ActionResult<IEnumerable<API_V_CERTIFPOINTAGE>>> GetAPI_V_CERTIFPOINTAGE()
         {
-            setDB(); return new List<API_V_CERTIFPOINTAGE>();//await _db.API_V_CERTIFPOINTAGE.ToListAsync();
+            setDB(); return await _db.API_V_CERTIFPOINTAGE.ToListAsync();
         }
 
 		[HttpGet("Date/{DateDebut}/{DateFin}")]
 		public async Task<ActionResult<IEnumerable<API_V_CERTIFPOINTAGE>>> GetAPI_V_CERTIFPOINTAGEByDate(DateTime DateDebut, DateTime DateFin)
 		{
-			setDB(); return new List<API_V_CERTIFPOINTAGE>();//await _db.API_V_CERTIFPOINTAGE.Where(a => a.Date >= DateDebut && a.Date <= DateFin).ToListAsync();
+			setDB(); return await _db.API_V_CERTIFPOINTAGE.Where(a => a.Date >= DateDebut && a.Date <= DateFin).ToListAsync();
         }
 
 
