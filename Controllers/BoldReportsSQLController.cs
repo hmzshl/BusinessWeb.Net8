82c0dde61d7921477df85a1afd13a45b47c9e969
Administrator <Administrator@PERSOVPS>
2025-11-26 14:29:30 +0100

    261125_1429

--- Controllers/BoldReportsSQLController.cs
+++ Controllers/BoldReportsSQLController.cs
@@ -1,13 +1,16 @@
 ﻿using BoldReports.Web;
 using BoldReports.Web.ReportViewer;
+using BusinessWeb.Data;
+using DocumentFormat.OpenXml.Bibliography;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using System.Collections.Generic;
 
 namespace BusinessWeb.Controllers
 {
-    [Route("api/{controller}/{ConnectionString}/{action}/{id?}")]
+    [Route("api/{controller}/{ste}/{action}/{id?}")]
     public class BoldReportsSQLController : ControllerBase, IReportController
     {
         // Report viewer requires a memory cache to store the information of consecutive client requests and
@@ -17,10 +20,12 @@ namespace BusinessWeb.Controllers
         // IHostingEnvironment used with sample to get the application data from wwwroot.
         private IWebHostEnvironment _hostingEnvironment;
         Dictionary<string, object> jsonResult = null;
-        public BoldReportsSQLController(IMemoryCache memoryCache, IWebHostEnvironment hostingEnvironment)
+        BusinessWebDBContext _sdb;
+        public BoldReportsSQLController(IMemoryCache memoryCache, IWebHostEnvironment hostingEnvironment, BusinessWebDBContext sdb)
         {
             _cache = memoryCache;
             _hostingEnvironment = hostingEnvironment;
+            _sdb = sdb;
         }
         //Get action for getting resources from the report
         [ActionName("GetResource")]
@@ -38,12 +43,12 @@ namespace BusinessWeb.Controllers
 			// Here, we have loaded the sales-order-detail.rdl report from the application folder wwwroot\Resources. sales-order-detail.rdl should be in the wwwroot\Resources application folder.
 			System.IO.FileStream reportStream = new System.IO.FileStream(basePath + @"\resources\" + reportOption.ReportModel.ReportPath + ".rdl", System.IO.FileMode.Open, System.IO.FileAccess.Read);
 			reportOption.ReportModel.Stream = reportStream;
-			Helpers fn = new Helpers();
+            Helpers fn = new Helpers();
 
 			DataSourceCredentials dataSourceCredentials = new DataSourceCredentials();
 			//You have to provide the shared data source name used with the report or the data source name available with the report.
 			dataSourceCredentials.Name = "DataSource1";
-			dataSourceCredentials.ConnectionString = RouteData.Values["ConnectionString"].ToString().Replace("FFFFFFF", "\\");
+            dataSourceCredentials.ConnectionString = fn.getDb(_sdb.TSocietes.Where(a => a.id == Int16.Parse(RouteData.Values["ste"].ToString())).SingleOrDefault())?.Database?.GetConnectionString(); ;
 			reportOption.ReportModel.DataSourceCredentials = new List<DataSourceCredentials> { dataSourceCredentials };
 		}
 		// Method will be called when report is loaded internally to start the layout process with ReportHelper.
